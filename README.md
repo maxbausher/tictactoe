ad5d4419ad89d0f417d0d05f33713cb8b3c9a8ce
maxbausher <maxbausher@gmail.com>
2017-06-02 14:02:51 +1000

    final updates comments and readme

--- README.md
+++ README.md
@@ -1,32 +1,39 @@
 
-link to live site and libraries used, mention features, (multiple rounds, rules of game, local storage, etc.)
+# Naughts & Crosses: Guardians of the Galaxy Style
 
-# Top Heading
-
-Here is some normal text
-
-Here is [my live site](http:fillmurray.com)
-
-![fill murray](http://fillmurray.com/400/300)
-
-Here is _italicicised text_.
-
-Here is **bold text**.
-
-## Smaller Heading
-
-- First item
-- Second item
+If you are a fan like me, you may enjoy killing some time by trying to battle your way into a treasured spot in this intergalactic crew of guardians.
 
 
 
-```js
-var bill = 'murray';
+Click here to have a try! [Naughts & Crosses: Guardian of the Galaxy Edition](https://maxbausher.github.io/tictactoe/)
 
-```
+_Description of Game Rules & Features below._
 
-______
+![fill murray](https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_UY1200_CR90,0,630,1200_AL_.jpg)
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-______
+## Game Description
+- Face off against your opponent in an epic battle of naughts & crosses.
+- Advance through the 12 available levels, until one of the players has one 6 rounds.
+- Why just 6 rounds? Well, there are only 6 types of Infinity Stones in existence. The players are on a race to collect them all before their opposition!
+
+## Rules
+
+- 1) Each players pick an avatar from the available characters displayed by simply clicking on the image.
+- 2) You can change your character before game play by simply clicking on the one you want to update it to.
+- 3) Once the first play is made, the players are locked in.
+- 4) Only a win will earn the player an infinity stone, and advance the game to the next level.
+- 5) Simply click **'Next Round'** to advance the game to the next round.
+
+## Features
+
+- 1) While the current state of the game is reset, if the browser is refreshed or closed, your infinity stones will be saved!
+
+- 2) If previous players' progress is saved, simply click **'Clear History'** to fully reset. Otherwise, play through the round through to a win in order to lock in previous game. Simply click **'Next Round'** to advance again.
+
+## Nuts & Bolts
+
+1) Using local storage to achieve this capability. May not work across all browsers (Chrome will work!)
+
+2) Libraries: jquery
+3) Using Starlord Font (see more details @ Top of CSS - 'tictactoe.css')
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,15 +1,15 @@
 
-var saveLocally = false;
+var saveLocally = false; //default false, if browser has functionality will change to true
 
 
-var game = {
+var game = {  //all game logic wrapped in game object
 
   players: {
 
     x: {
-      name: '',
-      score: 0,
-      image: ''
+      name: '',    //this will be assigned upon the on.click of the avatar image
+      score: 0,    //incremented upon a win condition - see checkWinner();
+      image: ''    //this will be a file path to images folder, assigned upon on.click of avatar img
     },
 
     o: {
@@ -19,25 +19,26 @@ var game = {
     }
 
   },
-  user: 'x',
-  turns: 0,
-  win: false,
-  userImagesSelected: 0,
-  gamesPlayed: 0,
-  playersSet: false,
+  user: 'x',   // default, first player starts as X
+  turns: 0,    // this counter will be incremented after ever click of <td> in my table (max: 9 turns)
+  win: false,  // default false, re-assigned true when checkWinner() returns true (see line 157)
+  userImagesSelected: 0,  //incremented after each click of an avatar - ensuring both players set.
+  gamesPlayed: 0,  //increments per win, determines switch of level background images (Lines 62 & 177)
+  playersSet: false, //used to skip the userImagesSelected counter after initial round (see line 139)
   levelBackgrounds:['images/moragstructure.jpg','images/novaprimebackground.jpg','images/kylnbackground.jpg','images/jailpic.jpeg','images/sanctuary.png','images/Knowhere.png','images/knowhereinside.jpg','images/finalbattle.jpg','images/darkastorcrash.png'],
-  // levelBackgrounds: [url('../images/moragplanet.jpg')],
+  // levelBackgrounds being looped through: [url('../images/moragplanet.jpg')],
 
   board: [null, null, null, null, null, null, null, null, null],
+  //default gameboard state - null : cell index
 
-  resetGame: function(){
+  resetGame: function(){ //executed upon click of "Next Round" button and if you refresh browser
     this.board = [null, null, null, null, null, null, null, null, null]
     this.turns = 0;
     this.win = false;
-    this.userImagesSelected = 2;
+    this.userImagesSelected = 2; // sets so that players do not have to reselect avatar images
   },
   checkWinner: function(movesArray){
-    // debugger;
+    // passing current board after each move & looking through all winning scenarios
     var board = this.board;
     if(board[0] === board[1] && board[0] === board[2] && board[0] !== null){return true;}
     if(board[3] === board[4] && board[3] === board[5] && board[3] !== null){return true;}
@@ -49,25 +50,24 @@ var game = {
     if(board[2] === board[4] && board[2] === board[6] && board[2] !== null){return true;}
 
       return false;
-  }, //checkWinner function
+  }, //checkWinner function, return true if X or O has returned for any of the winning scenarios
 
   updateUI: function(){
     if (this.win) {
-      // debugger;
-      console.log(game.players[game.user].name + " won");
+      // if win var = true, handle some UI related updates to tell users what is happening
       $('#resultbanner').fadeIn(1500);
       $('#resultbanner').text(game.players[game.user].name + ' wins!');
-      $('#clearHistory').hide();
-      game.players[game.user].score +=1;
-      game.gamesPlayed += 1;
-      $('#' + game.user + ' img:hidden:first').show();
+      $('#clearHistory').hide(); //hides option to clear last game history assuming win means continue
+      game.players[game.user].score +=1; //add to winner another score counting towards max of 6 wins.
+      game.gamesPlayed += 1; // kicks off level background change
+      $('#' + game.user + ' img:hidden:first').show(); //show another of the 6 hidden gem img's
 
-      //localStorage save of score
+      //localStorage of player object (scores, images, name) if functionality availalbe in browser
       if( saveLocally ){
         localStorage.setItem('gameState', JSON.stringify(game.players));
       }
 
-      if(game.players[game.user].score === 6){
+      if(game.players[game.user].score === 6){ //if this winning player has won 6 rounds, execute:
         $('body').css('background-image','url(images/finale.jpg)').fadeIn(4000);
         $('#resultbanner').css({'background-color': 'rgba(0,0,0, 0.75)','height': '150px'});
         $('#resultbanner').text(game.players[game.user].name + " is a Guardian of the Galaxy ").fadeIn(6000);
@@ -75,40 +75,39 @@ var game = {
         $('.fade').fadeOut(1500);
         if (saveLocally){
           localStorage.removeItem('gameState');
+          // if all 6 rounds completed, game is done, clear last game history out of browser's storage
         }
       }
 
-      // $('.playerRecord img:hidden:first')
     } else {
+        // if var win !== true, then by default game is a draw.
       console.log('game is a draw');
       $('#resultbanner').fadeIn(1500);
       $('#resultbanner').text('Draw');
     }
   }
-}; // var game
+};
 
 var restoreGameUI = function () {
-
-  // $( "#x img:hidden" ).each(function() {
+    //loops back through hidden gem images, and unhides 1 for each restored score unit of player
     var i =1;
     console.log(game.players.x.score);
-    while(i <= game.players.x.score){
-    //   $('#x img:nth-child(' + i + ')').show();
+    while(i <= game.players.x.score){ //for user x/player 1
+
       $('#x').children().eq(i).show();
       i++;
     }
-  // });
 
   $( "#o img:hidden" ).each(function() {
     var i = 1;
-    while(i <= game.players.o.score){
+    while(i <= game.players.o.score){ //for user o/player 2
       $('#o').children().eq(i).show();
       i++;
     }
   });
 
-  game.userImagesSelected = 2;
-  game.win = false;
+  game.userImagesSelected = 2;  //in restore need to reset var to 2 to pass check on avatar selection.
+  game.win = false;  //reset to false so restored players can commence new round
   $('#title1').text(this.id);
   $('#playerXbackground').css({'background-image': 'url(' + game.players.x.image + ')', 'background-size': 'cover'});
   $('#title2').text(this.id);
@@ -118,8 +117,6 @@ var restoreGameUI = function () {
 };
 
 $( document ).ready(function() {
-
-  // var bot = false;
 
   // Load saved game state if available
   if (typeof(Storage) !== "undefined") {
@@ -133,20 +130,21 @@ $( document ).ready(function() {
     }
   }
 
-  $( "td" ).on('click', function() {
-// debugger;
+  $( "td" ).on('click', function() {  //fires each time a player clicks a cell in tictactoe
+
     if(game.userImagesSelected === 0){
       alert("Player 1 needs to select a character, followed by Player 2");
       return;
     } else if (game.userImagesSelected === 1){
       alert("Player 2 still needs to select a character");
+      return;
     } else {
      game.playersSet = true;
-     $('.characterSelector *').hide();
+     $('.characterSelector *').hide(); //hide other avatar images after selections are made
     }
-    var boardIndex = this.id;
+    var boardIndex = this.id; // save id of td (which is same as it's index in game.board)
 
-    if( game.win ){
+    if( game.win ){  //ensures if round is one that players cannot continue interacting with the board
       return;
     }
 
@@ -155,16 +153,19 @@ $( document ).ready(function() {
       return;
     }
 
-    game.board[ boardIndex ] = game.user; //find
+    game.board[ boardIndex ] = game.user;
+    //natural else if location in board is null, fill it with "X" or "O"
     $("#" + boardIndex).css('background-image', "url(" + game.players[game.user].image + ")");
-    game.turns += 1;
+    // sets background of resepctive cell to image of player's avatar
+    game.turns += 1;  // increments turn counter by one so we can keep track of 9 moves.
 
-    game.win = game.checkWinner(game.board);
+    game.win = game.checkWinner(game.board); // sets win var to true if winning scenario matched.
     if(game.win || game.turns === 9) {
-      game.updateUI();
+    //either 'true' (aka win) || or draw as all 9 turns taken w/ no win
+      game.updateUI(); //call update UI function with corresponding result
       return;  // avoid switching player token after win
     }
-
+    //below if/else is toggling from original default of 'x' to 'o', and back to 'x' per turn/on.click of td
     if( game.user === 'x' ) {
       game.user = 'o'
     } else {
@@ -174,51 +175,46 @@ $( document ).ready(function() {
 
 
   $('#nextLevel').on('click', function(){
+      //clears backend game logic (see line 34)
       game.resetGame();
-      // $('body').css('background-image', "url(" + game.levelBackgrounds + ")");
+      // below jquery resets the front-end display: clears cells of images/hides h2 result banner
       $('td').css('background-image', '');
       $('#resultbanner').hide();
       $('#resultbanner').text('');
       if(game.gamesPlayed > 0){
           $('body').css('background-image', "url(" + game.levelBackgrounds[game.gamesPlayed - 1] + ")");
-      };
-      // $('img').css
+      }
+
   });
 
   $('#clearHistory').on('click', function(){
-
-      localStorage.removeItem('gameState');
-      game.resetGame();
-      game.userImagesSelected = 0;
-      game.players.x.score = 0;
+      // clear history button only appears if localStorage has been used:
+      localStorage.removeItem('gameState'); // clears localStorage as per user request
+      game.resetGame(); // resets game logic on back end
+      game.userImagesSelected = 0; //resets checks on avatar selection in on.click('td')
+      game.players.x.score = 0; //resets scores from prior game to 0
       game.players.o.score = 0;
-      $('#playerXbackground, #playerObackground').css('background-image', '');
-      $('#x img, #o img, #clearHistory').hide();
+      $('#playerXbackground, #playerObackground').css('background-image', '');  // resets player divs
+      $('#x img, #o img, #clearHistory').hide(); // resets player divs
 
 
   });
 
 
-  $('.characterSelector > img').on('click', function() {
-      game.userImagesSelected += 1;
-      game.players[game.user].name = this.id
+  $('.characterSelector > img').on('click', function() { //selection of avatar on.click of image
+      game.userImagesSelected += 1; //increment each time to ensure we track selection & correct alert
+      game.players[game.user].name = this.id //sets player objects 'x' & 'o' properties
       game.players[game.user].image = this.src
-      $('#' + this.id).hide();
+      $('#' + this.id).hide(); // hides selected image from avatar div once picked
       if( game.user === 'x' ){
-        $('#title1').text(this.id);
-        $('#playerXbackground').css({'background-image': 'url(' + game.players.x.image + ')', 'background-size': 'cover'});
-        game.user = 'o'
+        $('#title1').text(this.id); //changes player 1 text to ID name on image selected
+        $('#playerXbackground').css({'background-image': 'url(' + game.players.x.image + ')', 'background-size': 'cover'}); //changes background of div to avatar image and sets to cover;
+        game.user = 'o' // switches user to 'o' so next img selection sets up in player.o.name/img/etc
       } else {
         $('#title2').text(this.id);
         $('#playerObackground').css({'background-image': 'url(' + game.players.o.image + ')', 'background-size': 'cover'});
-        game.user = 'x'
+        game.user = 'x' //sets back to 'x', ensuring 1st click on table/td is associated to "x"'s move
       }
   });
-
-
-  $('#cheat').on('click', function () {
-    game.players.x.score = 5;
-    $('#0,#3,#1,#4,#2,#5,#8').click();
-  })
 
 }); //doc.ready
